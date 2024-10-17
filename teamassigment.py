327ebef3f25bea6dfe3116558e0dc0ac4482fa0e
Cristian Garcia <153207626+Chris20385@users.noreply.github.com>
2024-10-17 12:14:04 -0500

    Update teamassigment.py

--- teamassigment.py
+++ teamassigment.py
@@ -4,4 +4,9 @@ options = (
   "3. Make Breakfast",
   "4. Go to the gym",
   "5. Take care of sibling",
-  "6. Exit"
+  "6. Exit")
+
+def output_options():
+  print ("What option will you be choosing:")
+  for option in options:
+    print (option)
