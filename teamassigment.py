ca938dd640cf592e9c569a29240b7e3d73f7a647
Cristian Garcia <153207626+Chris20385@users.noreply.github.com>
2024-10-19 16:11:25 -0500

    Update teamassigment.py

--- teamassigment.py
+++ teamassigment.py
@@ -10,3 +10,16 @@ def output_options():
   print ("What option will you be choosing:")
   for option in options:
     print (option)
+
+def main():
+  while True:
+    output_options()
+
+    option = input("Choose and enter the number of your choice:")
+
+    if option == "1":
+      print ("You are going to school")
+    elif option == "2":
+      print("Nice! you are going to playing video games")
+    elif option == "3":
+      print("Your hungry lets make breakfast")
